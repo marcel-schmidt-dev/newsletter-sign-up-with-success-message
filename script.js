50aeb2256c9970114f90074e11e571b4e92335e3
Marcel Schmidt <marcelschmidt1989@live.de>
2024-06-06 18:58:40 +0200

    addad functionality, validation

--- script.js
+++ script.js
@@ -1,13 +1,37 @@
 const validation = new JustValidate('#form');
+const form = document.getElementById('form');
+const container = document.querySelector('.container');
+const dismissbtn = document.querySelector('.success button');
+const success = document.querySelector('.success');
 
-validation
-    .addField('#email', [
-        {
-            rule: 'required',
-            errorMessage: 'valid email is required',
-        },
-        {
-            rule: 'email',
-            errorMessage: 'email is invalid!',
-        },
-    ]);
+validation.addField('#email', [
+    {
+        rule: 'required',
+        errorMessage: 'valid email is required',
+    },
+    {
+        rule: 'email',
+        errorMessage: 'email is invalid!',
+    },
+]);
+
+const handleSubmit = (e) => {
+    e.preventDefault(e);
+    if (validation.isValid) {
+        const formData = new FormData(e.target);
+        const data = Object.fromEntries(formData);
+        const mail = success.querySelector('p span');
+        mail.innerHTML = data.email
+        container.style.display = 'none';
+        success.style.display = 'block';
+
+    }
+};
+
+const handleDismiss = () => {
+    container.style.display = 'flex';
+    success.style.display = 'none';
+}
+
+form.addEventListener('submit', handleSubmit);
+dismissbtn.addEventListener('click', handleDismiss)
